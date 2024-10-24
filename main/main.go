112459baafc0a1027e5b8ebcf36fb8433e99dac0
jacob <jacobinca19@gmail.com>
2024-10-24 16:03:19 +0800

    feat: provide a http server

--- main/main.go
+++ main/main.go
@@ -12,11 +12,9 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", helloHandler)
 
-	// 设置监听端口
 	port := ":8088"
 	fmt.Printf("Starting server on %s\n", port)
 
-	// 启动 HTTP 服务
 	if err := http.ListenAndServe(port, nil); err != nil {
 		fmt.Println("Error starting server:", err)
 	}

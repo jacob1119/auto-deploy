3b7a94d5ec3738353c8a5b818d69ab8964ef05dc
jacob <jacobinca19@gmail.com>
2024-10-24 20:39:26 +0800

    deploy: ci deploy

--- main/main.go
+++ main/main.go
@@ -9,8 +9,13 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "fuck you!!!")
+}
+
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/hello", helloWorldHandler)
 
 	port := ":8088"
 	fmt.Printf("Starting server on %s\n", port)
